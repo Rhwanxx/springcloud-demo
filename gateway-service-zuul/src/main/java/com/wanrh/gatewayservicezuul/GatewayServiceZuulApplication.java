9f56ecfdd4b0e9fb423b98167bbc47a5f5142024
Rhwan <13072760107@163.com>
2019-04-10 11:02:40 +0800

    zuul 网关

---
+++ gateway-service-zuul/src/main/java/com/wanrh/gatewayservicezuul/GatewayServiceZuulApplication.java
@@ -0,0 +1,15 @@
+package com.wanrh.gatewayservicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceZuulApplication.class, args);
+    }
+
+}
